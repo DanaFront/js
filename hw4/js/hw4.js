082c680c3bf6824fb7423b7cefb5e199a5fbbcc0
DanaFront <makhanovadana@gmail.com>
2020-05-18 11:17:35 +0300

    дз 4

---
+++ hw4/js/hw4.js
@@ -0,0 +1,20 @@
+// Задача 1
+
+let num = prompt("Введите любое число");
+let res = 0;
+
+for ( let i = 0; i < num.length; i++) {
+    res += Number(num[i]);
+}
+
+console.log(res);
+
+
+// Задача 2
+
+let str = "17 05 2020";
+console.log(str.replace(/0/ig, "00"));
+
+
+// Задача 3 в index.html
+
